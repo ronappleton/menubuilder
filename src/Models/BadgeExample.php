1df8e65ecbf832f9b86a2c99a994cfb52aa587e7
Ron Appleton <ron.appleton@gmail.com>
2017-12-20 02:24:30 +0000

    Example Model
    Example BadgeModel

---
+++ src/Models/BadgeExample.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RonAppleton\MenuBuilder\Models;
+
+class BadgeExample
+{
+    public function unreadEmails()
+    {
+        return 216;
+    }
+
+    public function totalUser()
+    {
+        return 12093;
+    }
+}
